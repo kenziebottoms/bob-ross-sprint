13cb28163f464823ced77998c84e711900178289
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-01 10:37:44 -0500

    config passport strat

--- server.js
+++ server.js
@@ -4,17 +4,38 @@ const express = require("express");
 const app = express();
 const movieApi = require("imdb-api");
 
+// static routes
 app.use(express.static(__dirname + "/client"));
-app.use('/angular', express.static(__dirname+'/node_modules/angular'));
-app.use('/angular-route', express.static(__dirname+'/node_modules/angular-route'));
+app.use('/angular', express.static(__dirname + '/node_modules/angular'));
+app.use('/angular-route', express.static(__dirname + '/node_modules/angular-route'));
 
+// auth
+const session = require('express-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+require('./server/config/passport-strat.js');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(require('./server/routes'));
+
+// TODO: move into routes/
+// api endpoints
 app.get("/movies/:q", (req, res, next) => {
   movieApi
     .search(
       { title: req.params.q },
       { apiKey: require("./server/key").key }
     )
-    .then(({results}) => {
+    .then(({ results }) => {
       res.status(200).json(results);
     })
     .catch(err => {
